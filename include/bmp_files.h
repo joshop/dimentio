3ba6e94ebdaf86595351e9b1cacc19ada03bf3c0
joshop <joshop4646@gmail.com>
2023-07-31 21:37:07 -0400

    giga ultra

---
+++ include/bmp_files.h
@@ -0,0 +1,6 @@
+#include "helper.h"
+extern const COLOR *bmp_grass;
+extern const COLOR *bmp_papermario;
+extern const char *bmps_mario;
+extern const char *bmps_mario_fall;
+extern const COLOR *bmp_background;
